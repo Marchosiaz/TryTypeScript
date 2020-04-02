d435a78ed4266d1465a8f645c26ffed70744fd45
Maxim <mark.hruzin@gmail.com>
2020-04-03 00:10:00 +0400

    Arrays in core types

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var person = {
     name: 'Maximus',
-    age: 39
+    age: 39,
+    hobbies: [
+        'coding',
+        'watching videos about psychology, programming and others',
+        'read books',
+        'play chess',
+        12
+    ]
 };
-console.log(person.name);
+console.log(person);
--- app.ts
+++ app.ts
@@ -1,9 +1,17 @@
 const person: {
     name: string;
     age: number;
+    hobbies: any[];
 } = {
     name: 'Maximus',
     age: 39,
+    hobbies: [
+        'coding',
+        'watching videos about psychology, programming and others',
+        'read books',
+        'play chess',
+        12
+    ]
 };
 
-console.log(person.name);
+console.log(person);
