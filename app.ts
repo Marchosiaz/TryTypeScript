15fbea57e0304e5581657b409f44e4e53a8e5551
Maxim <mark.hruzin@gmail.com>
2020-04-02 23:05:09 +0400

    renove src and public folders from my project

---
+++ app.ts
@@ -0,0 +1,8 @@
+function add(num1, num2) {
+    return num1 + num2;
+};
+
+const number1 = 123;
+const number2 = 414;
+
+console.log(add(number1, number2))
--- src/testTS.js
+++
@@ -1,9 +0,0 @@
-var button = document.querySelector("button");
-var input1 = document.getElementById("num1");
-var input2 = document.getElementById("num2");
-var add = function (num1, num2) {
-    return num1 + num2;
-};
-button.addEventListener("click", function () {
-    console.log(+input1.value, +input2.value);
-});
