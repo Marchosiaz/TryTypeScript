10f89fd7fd4e97ee27d19c0d2d679967636062b5
Maxim <mark.hruzin@gmail.com>
2020-04-03 16:21:24 +0400

    function types and callbacks

--- app.js
+++ app.js
@@ -4,3 +4,14 @@ var add = function (n1, n2) {
 var printResult = function (num) {
     console.log('Hello is ' + num);
 };
+var addAndHandle = function (n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+};
+printResult(add(5, 12));
+var combineValues;
+combineValues = add;
+console.log(combineValues(8, 12));
+addAndHandle(1, 312, function (result) {
+    console.log(result);
+});
--- app.ts
+++ app.ts
@@ -5,3 +5,20 @@ const add = (n1: number, n2: number) => {
 const printResult = (num: number): void => {
     console.log('Hello is ' + num)
 }
+
+const addAndHandle = (n1: number, n2: number, cb: (num: number) => void) => {
+    const result = n1 + n2;
+    cb(result)
+}
+
+printResult(add(5, 12));
+
+let combineValues: (a: number, b: number) => number;
+
+combineValues = add;
+
+console.log(combineValues(8, 12));
+
+addAndHandle(1, 312, (result) => {
+    console.log(result);
+})
